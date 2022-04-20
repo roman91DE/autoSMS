6fdec1faf0e2fc7efbe49e2fd8402fef92eebc08
rmn@mbp <rohoehn123@gmail.com>
2022-04-20 18:34:09 +0200

    init commit

---
+++ run.py
@@ -0,0 +1,35 @@
+#!/data/data/com.termux/files/usr/bin/python3
+
+from argparse import ArgumentError
+from sys import argv, stderr
+from subprocess import run
+from json import load
+
+
+def main():
+
+    if len(argv) != 3:
+        print(
+            f"Wrong Number of arguments, needs path to <contacts.json> and <message.txt>",
+            file=stderr,
+        )
+        raise ArgumentError
+
+    _, CONTACTS_PATH, MESSAGE_PATH = argv
+
+    contacts = load(open(CONTACTS_PATH, "r"))
+
+    number_str = ""
+    for contact in contacts:
+        number_str += (f"{contact['number']},").replace("+49", "0")
+
+    message = open(MESSAGE_PATH, "r").readlines()
+
+    try:
+        run(["termux-sms-send", "-n", number_str[:-1], 1, message])  # slot of sim card
+    except:
+        print("Something went wrong :-(", file=stderr)
+
+
+if __name__ == "__main__":
+    main()
